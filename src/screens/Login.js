f59234fa2ba22036c74ef57781c4073ff8442140
BaeSungMin <qo98333@naver.com>
2021-10-12 17:03:14 +0900

    Login Page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,45 +1,81 @@
-import React, { useState } from 'react';
+// 로그인 네비게이션
+
+import React, { useState, useRef, useEffect } from 'react';
+import { TouchableWithoutFeedback,Keyboard } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styled from 'styled-components/native';
-//import { Text, Button } from 'react-native';
-import { Image, Input } from '../components';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Image, Input, Button } from '../components';
 import { images } from '../utils/images'
 
-// 로그인 화면
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 
 const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
     background-color: ${ ({theme}) => theme.background };
-    padding: 20px;
+    padding: 0 20px;
+    padding-top: ${({ insets: {top} }) => top}px;
+    padding-bottom: ${({ insets: {bottom} }) => bottom}px;
 `;
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+    const [disabled, setDisabled] = useState(true);
+    const passwordRef = useRef();
+    const insets = useSafeAreaInsets();   
+
+    const _handleLoginButtonPress = () => {};
+    useEffect(() => {
+        setDisabled(!(email && password ));
+    }, [email, password]);
+
     return(
-        <Container>
-            <Image url={images.logo} imageStyle={{ borderRadius: 8 }}/>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                onSubmitEditing = {() => {}}
-                placeholder = "Email"
-                returnKeyType = "next"
-            />
-            <Input
-                label="password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                onSubmitEditing = {() => {}}
-                placeholder = "Password"
-                returnKeyType = "done"
-                isPassword
-            />
-        </Container>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            extraScrollHeight={20}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container insets={insets}>
+                    <Image url={images.logo} imageStyle={{ borderRadius: 8 }}/>
+                    <Input
+                        label="Id"
+                        value={email}
+                        onChangeText={text => setEmail(text)}
+                        onSubmitEditing = {() => passwordRef.current.focus()}
+                        placeholder = "Id"
+                        returnKeyType = "next"
+                    />
+                    <Input
+                        ref={passwordRef}
+                        label="Password"
+                        value={password}
+                        onChangeText={text => setPassword(text)}
+                        onSubmitEditing = {() => {}}
+                        placeholder = "Password"
+                        returnKeyType = "done"
+                        isPassword
+                    />
+                    <Button 
+                        title="Login" 
+                        onPress={_handleLoginButtonPress} 
+                        disabled={disabled}
+                    />
+                    <Button
+                        title="Sign up"
+                        onPress={() => navigation.navigate('Signup')}
+                        isFilled={false}
+                    />
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAwareScrollView>    
     );
 };
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 
 export default Login;
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,6 @@
 import Login from './Login';
 import Signup from './Signup';
 
+/* import ... from './components'; 이렇게 screen디렉토리의 component를 import할때
+   경로를 components로 잡기 위해 , 이렇게 하면 javascript는 찾아서 해준다. */
 export { Login, Signup };
