0e5eb6d6259a57c63180218346d952258177705c
sonjh <7dot83hz@gmail.com>
2021-10-15 00:21:49 +0900

    feat(Community Page Button, Post Page)

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,8 +2,10 @@ import Login from './Login';
 import Signup from './Signup';
 import Home from './Home';
 import Board from './Home';
+import Community from './Community';
+import Post from './Post';
 
 /* import ... from './components'; 이렇게 screen디렉토리의 component를 import할때
    경로를 components로 잡기 위해 , 이렇게 하면 javascript는 찾아서 해준다. */
 
-export { Login, Signup, Home, Board };
+export { Login, Signup, Home, Board, Community, Post };
