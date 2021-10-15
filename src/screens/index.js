9e5d94d48ff42c919f4159c405503ac705dd0cb0
sonjh <7dot83hz@gmail.com>
2021-10-15 15:51:28 +0900

    feat(PostEditor)

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,10 +2,11 @@ import Login from './Login';
 import Signup from './Signup';
 import Home from './Home';
 import Board from './Home';
-import Community from './Community';
-import Post from './Post';
+import Community from './community/Community';
+import Post from './community/Post';
+import PostEditor from './community/PostEditor';
 
 /* import ... from './components'; 이렇게 screen디렉토리의 component를 import할때
    경로를 components로 잡기 위해 , 이렇게 하면 javascript는 찾아서 해준다. */
 
-export { Login, Signup, Home, Board, Community, Post };
+export { Login, Signup, Home, Board, Community, Post, PostEditor };
