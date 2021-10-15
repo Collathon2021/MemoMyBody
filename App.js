714892cce2ea2db72b1b3a497233f702af61e8e2
sonjh <7dot83hz@gmail.com>
2021-10-16 00:35:36 +0900

    redux install. setting 덜 함.

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
 import App from './src/App';
-
-export default App;
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+export default class App extends Component {
+    render() {
+      return (
+        <Provider store={store}>
+          <div>
+            <Person />
+          </div>
+        </Provider>
+      );
+    }
+  }
