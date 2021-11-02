88a00a8fe79f1767f3c33914f45e90157b2a0aaf
BaeSungMin <qo98333@naver.com>
2021-11-03 01:11:15 +0900

    require DB connect

--- test/api.js
+++ test/api.js
@@ -1,8 +1,8 @@
 export default {
-      apiKey:<YOUR APIKEY>,
-      appId:"1:21995980128:android:2dad0184d5e58bb6558a05",
-      databaseURL: "https://collathon-default-rtdb.asia-southeast1.firebasedatabase.app",
-      messagingSenderId:<YOUR MESSAGINGSENDER ID>,
-      projectId:"collathon",
-      storageBucket:"gs://collathon.appspot.com"
-    }
+  apiKey:"AIzaSyAtfQa9O5-C4GeuBpZsnJa3PQeXwExFo2Y",
+  appId:"1:21995980128:android:2dad0184d5e58bb6558a05",
+  databaseURL: "https://collathon-default-rtdb.asia-southeast1.firebasedatabase.app",
+  messagingSenderId:"21995980128",
+  projectId:"collathon",
+  storageBucket:"gs://collathon.appspot.com"
+}
