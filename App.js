001d56543dc19a55ede904648338e7bf81db416e
inhyuk15 <15inhyuk@gmail.com>
2021-11-07 21:32:06 +0900

    합침

--- App.js
+++ App.js
@@ -16,13 +16,6 @@ export default class App extends Component {
   }
   */
 
-import App from './src/App';
-import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-export default class app extends Component {
-    render() {
-      return (
-        <App/>
-      );
-    }
-  }
+  import App from './src/App';
+
+  export default App;
