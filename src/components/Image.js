5adb6fb9409cbf1a352a7c58d8fd07d21096428f
inhyuk15 <15inhyuk@gmail.com>
2021-11-10 20:56:42 +0900

    Revert "Revert "Revert "Revert "re""""
    This reverts commit 12922229443bd4d254a833a679348cb726d57b1d.

---
+++ src/components/Image.js
@@ -0,0 +1,43 @@
+// Image 컴포넌트 (이미지 사용가능)
+
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
+
+const Container = styled.View`
+    align-self: center;
+    margin-bottom: 30px;
+`;
+
+const StyledImage = styled.Image`
+    background-color: ${({ theme }) => theme.imageBackground};
+    width: 100px;
+    height: 100px;
+    border-radius: ${({ rounded }) => (rounded ? 50 : 0)}px;
+`;
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
+
+const Image = ({ url, imageStyle, rounded }) => {
+    return(
+        <Container>
+            <StyledImage source={{ uri: url }} style={imageStyle} rounded={rounded}/>
+        </Container>
+    );
+};
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
+
+Image.defaultProps = {
+    rounded: false,
+};
+
+Image.propTypes = {
+    uri: PropTypes.string,
+    imageStyle: PropTypes.object,
+    rounded: PropTypes.bool,
+};
+
+export default Image;
