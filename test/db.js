327266b89cce62e24aa15479a5dd41b0cb0d3664
inhyuk15 <15inhyuk@gmail.com>
2021-11-10 20:56:50 +0900

    Revert "Revert "Revert "Revert "Revert "re"""""
    This reverts commit 5adb6fb9409cbf1a352a7c58d8fd07d21096428f.

--- test/db.js
+++
@@ -1,7 +0,0 @@
-import firebase from '@react-native-firebase/app'
-import '@react-native-firebase/firestore'
-import api from './api'
-
-
-const firebaseApp = !firebase.apps.length?firebase.initializeApp(api):firebase.app()
-export default firebaseApp.firestore() 
