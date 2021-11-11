9a9a389e63c88f694b477a4e41a79a093eb96ee3
BaeSungMin <qo98333@naver.com>
2021-11-11 21:22:23 +0900

    11/11 TODO2

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Login, Signup, Home, Menu, Community} from '../screens';
+import UserContextProvider from '../contexts/UserContext';
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 
@@ -13,39 +14,41 @@ const AuthStack = () => {
     const theme = useContext(ThemeContext);
 
     return(
-        <Stack.Navigator
-            initialRouteName="Login"
-            screenOptions={{
-                headerTitleAlign: 'center',
-                cardStyle: { backgroundColor: theme.backgroundColor },
-                headerTintColor: theme.headerTintColor,
-            }}
-        >
-            <Stack.Screen 
-                name="Login" 
-                component={Login}
-                //options={{ headerShown: false }}  회원가입 위에 Login 타이틀 없애기            
-            />
-            <Stack.Screen 
-                name="Signup" 
-                component={Signup}
-                options={{ headerBackTitleVisible: false }}
-            />         
-            <Stack.Screen
-                name="Home"
-                component={Home}
-            />
-            
-            <Stack.Screen
-                name="Community"
-                component={Community}
-            />
-            <Stack.Screen
-                name="Menu"
-                component={Menu}
-            />
-            
-        </Stack.Navigator>
+        <UserContextProvider>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerTitleAlign: 'center',
+                    cardStyle: { backgroundColor: theme.backgroundColor },
+                    headerTintColor: theme.headerTintColor,
+                }}
+            >
+                <Stack.Screen 
+                    name="Login" 
+                    component={Login}
+                    //options={{ headerShown: false }}  회원가입 위에 Login 타이틀 없애기            
+                />
+                <Stack.Screen 
+                    name="Signup" 
+                    component={Signup}
+                    options={{ headerBackTitleVisible: false }}
+                />         
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                />
+                
+                <Stack.Screen
+                    name="Community"
+                    component={Community}
+                />
+                <Stack.Screen
+                    name="Menu"
+                    component={Menu}
+                />
+                
+            </Stack.Navigator>
+      </UserContextProvider>
     );
 };
 
